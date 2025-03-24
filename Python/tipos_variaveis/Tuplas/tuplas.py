77c36de1c3a4fc86469c8f03a3b604db68618d5a
Paulo Cesar <pauloandradeoliveira2004@gmail.com>
2025-03-24 16:24:39 -0300

    estudo sobre tuplas, listas

---
+++ Python/tipos_variaveis/Tuplas/tuplas.py
@@ -0,0 +1,16 @@
+# Criando minha primeira tupla.
+tupla_exemplos = (1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+
+
+# Método COUNT
+ # - Verifica quantas vezes o número 3 aparece na tupla.
+tupla_exemplos.count(3) # Saída: 2
+print(tupla_exemplos)
+
+# Método INDEX
+    # - Verifica a posição do número 3 na tupla.
+print(tupla_exemplos.index(4)) # Saída: 3
+
+# Método LEN
+    # - Verifica o tamanho da tupla.
+print(len(tupla_exemplos)) # Saída: 10
